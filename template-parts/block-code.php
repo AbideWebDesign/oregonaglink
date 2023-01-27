e58399ed354ef5f3f0ecb8c7ee4cc380ae7210fd
Josh Armentano <josh@abidewebdesign.com>
2023-01-27 08:18:51 -0800

    no message

--- template-parts/block-code.php
+++ template-parts/block-code.php
@@ -1,7 +1,7 @@
 <div class="py-2">			
 	<div class="row">
 		<div class="col-12">
-			<div class="bg-white p-4">					
+			<div class="bg-white p-2">					
 				<?php the_sub_field('code'); ?>				
 			</div>
 		</div>
